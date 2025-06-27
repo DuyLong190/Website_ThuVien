8f24a890629128f92b4fcafc9f4cc06e6a5454ab
xuanquang1133 <xuanquang1133@gmail.com>
2025-06-27 21:39:42 +0700

    final

--- obj/Debug/net8.0/QuanPhucLongQuang_DoAnWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuanPhucLongQuang_DoAnWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanPhucLongQuang_DoAnWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bf53079a2890d23211e5d7f6f57f91ca6e9dd92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+570d9a07a396520aa008ad7c402733c3b120dc7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanPhucLongQuang_DoAnWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanPhucLongQuang_DoAnWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
